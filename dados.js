5632d3ce4ef855c5ee16000fb8efd9b064d55d92
Carolina Lira <100692089+carolinatlira@users.noreply.github.com>
2024-09-14 19:06:01 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+        {
+        titulo : "Rayssa Leal",
+        descricao : "A fadinha do skate, Rayssa Leal, conquistou o mundo com seu talento precoce e carisma. Com apenas 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, trazendo o bronze na modalidade street.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags:"skate fada sk8"
+        },
+        {
+            titulo : "Rebeca Andrade",
+            descricao : "Rebeca Andrade, a joia da ginástica artística brasileira, conquistou o coração dos brasileiros com sua determinação e talento. Bicampeã olímpica, ela é a maior medalhista da história do Brasil nos Jogos Olímpicos, acumulando diversas medalhas de ouro, prata e bronze. Sua trajetória inspiradora, marcada por superação e dedicação, a tornou um símbolo nacional. Com sua força e elegância, Rebeca revolucionou a ginástica no país e continua a deixar sua marca nas competições internacionais, sendo um exemplo para as futuras gerações de atletas.",
+            link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+            tags:"judô judo judoca judoka"
+        },
+        {
+            titulo : "Beatriz Souza",
+            descricao : "Beatriz Souza, a judoca brasileira, é um dos grandes nomes do judô feminino mundial. Com técnicas precisas e muita determinação, ela conquistou diversas medalhas em competições internacionais, sendo um dos principais destaques da modalidade no Brasil.",
+            link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+            tags:"ginástica ginastica ouro"
+        }];
